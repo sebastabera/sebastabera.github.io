0b90a124fa2c8dc926d42a9946922b1b6242707a
sebastabera <sebas03700@hotmail.com>
2016-04-12 21:14:47 -0500

    commit final

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+	google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+      var table1 = [
+					[5, 50],
+					[20, 40],
+					[75, 10]
+				];
+
+	var table = [
+          ['Navegador', 'Usuarios por día'],
+          ['IE6', 0],
+          ['IE7', 0],
+          ['IE8', 0]
+        ];
+	var aux = 0;
+      function drawChart() {
+
+      	for (var i = 0; i < table1.length; i++) {
+      		table[i+1][1] = table1[i][aux];
+      	};
+      	if (aux == 0) {
+      		aux == aux++;
+      	}else{
+      		aux = 0;
+      	}
+
+        var data = google.visualization.arrayToDataTable(table);
+
+        var options = {
+          title: 'Usuarios Internet Explorer'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    $('#cambioValores').click(function(){
+		drawChart();	 	
+	});
+	
+	
+});
+
+
